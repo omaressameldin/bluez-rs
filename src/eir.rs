cc91569e0cd35147a54f80a90d53beff697ffe36
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2020-12-02 21:39:46 +0000

    Experiment: Changing EIR parser to Vec instead of struct return type.

--- src/eir.rs
+++ src/eir.rs
@@ -50,30 +50,15 @@ pub struct ManufacturerSpecificData {
 }
 
 #[derive(Debug)]
-pub struct EIR {
-    flags: Option<BitFlags<EIRFlags>>,
-    uuid16: Vec<u16>,
-    uuid32: Vec<u32>,
-    uuid128: Vec<u128>,
-    name: Option<EIRName>,
-    tx_power_level: Vec<i8>,
-    uri: Vec<String>,
-    manufacturer_specific_data: Vec<ManufacturerSpecificData>,
-}
-
-impl EIR {
-    fn new() -> Self {
-        EIR {
-            flags: None,
-            uuid16: Vec::new(),
-            uuid32: Vec::new(),
-            uuid128: Vec::new(),
-            name: None,
-            tx_power_level: Vec::new(),
-            uri: Vec::new(),
-            manufacturer_specific_data: Vec::new(),
-        }
-    }
+pub enum EIR {
+    Flags(BitFlags<EIRFlags>),
+    Uuid16(Vec<u16>),
+    Uuid32(Vec<u32>),
+    Uuid128(Vec<u128>),
+    Name(String, bool),
+    TxPowerLevel(Vec<i8>),
+    Uri(Vec<String>),
+    ManufacturerSpecificData(Vec<ManufacturerSpecificData>),
 }
 
 #[derive(Error, Debug)]
@@ -110,8 +95,11 @@ enum EIRDataTypes {
 ///
 /// This will silently skip any unknown data types or URIs using
 /// encoded schemes.
-pub fn parse_eir<T: Buf>(mut buf: T) -> Result<EIR, EIRError> {
-    let mut eir = EIR::new();
+pub fn parse_eir<T: Buf>(mut buf: T) -> Result<Vec<EIR>, EIRError> {
+    let mut eir : Vec<EIR> = Vec::new();
+    let mut has_flag = false;
+    let mut has_name = false;
+    let mut uuid16_idx : Option<usize> = None;
 
     while buf.has_remaining() {
         // Bluetooth Specification Version 5.2, Vol 3, part C, 8 EXTENDED INQUIRY RESPONSE DATA FORMAT
@@ -135,10 +123,11 @@ pub fn parse_eir<T: Buf>(mut buf: T) -> Result<EIR, EIRError> {
         // EIRDataType values https://www.bluetooth.com/specifications/assigned-numbers/generic-access-profile/
         match FromPrimitive::from_u8(data_type) {
             Some(EIRDataTypes::Flags) => {
-                if eir.flags.is_some() {
+                if has_flag {
                     return Err(EIRError::RepeatedFlag);
                 }
-                eir.flags = Some(BitFlags::from_bits_truncate(data.get_u8()));
+                has_flag = true;
+                eir.push(EIR::Flags(BitFlags::from_bits_truncate(data.get_u8())));
             }
             Some(EIRDataTypes::UUID16Incomplete) | Some(EIRDataTypes::UUID16Complete) => {
                 if data.remaining() % 2 != 0 {
@@ -146,73 +135,83 @@ pub fn parse_eir<T: Buf>(mut buf: T) -> Result<EIR, EIRError> {
                         len: data.remaining(),
                     });
                 }
-                while data.has_remaining() {
-                    eir.uuid16.push(data.get_u16_le());
+                if uuid16_idx.is_none() {
+                    uuid16_idx = Some(eir.len());
+                    eir.push(EIR::Uuid16(Vec::new()));
+                }
+                if let EIR::Uuid16(mut uuid16data) = &eir[uuid16_idx.unwrap()] {
+                    while data.has_remaining() {
+                        uuid16data.push(data.get_u16_le());
+                    }
                 }
             }
-            Some(EIRDataTypes::UUID32Incomplete) | Some(EIRDataTypes::UUID32Complete) => {
-                if data.remaining() % 4 != 0 {
-                    return Err(EIRError::UnexpectedDataLength {
-                        len: data.remaining(),
-                    });
-                }
-                while data.has_remaining() {
-                    eir.uuid32.push(data.get_u32_le());
-                }
-            }
-            Some(EIRDataTypes::UUID128Incomplete) | Some(EIRDataTypes::UUID128Complete) => {
-                if data.remaining() % 16 != 0 {
-                    return Err(EIRError::UnexpectedDataLength {
-                        len: data.remaining(),
-                    });
-                }
-                while data.has_remaining() {
-                    eir.uuid128.push(data.get_u128_le());
-                }
-            }
+            // Some(EIRDataTypes::UUID32Incomplete) | Some(EIRDataTypes::UUID32Complete) => {
+            //     if data.remaining() % 4 != 0 {
+            //         return Err(EIRError::UnexpectedDataLength {
+            //             len: data.remaining(),
+            //         });
+            //     }
+            //     while data.has_remaining() {
+            //         eir.uuid32.push(data.get_u32_le());
+            //     }
+            // }
+            // Some(EIRDataTypes::UUID128Incomplete) | Some(EIRDataTypes::UUID128Complete) => {
+            //     if data.remaining() % 16 != 0 {
+            //         return Err(EIRError::UnexpectedDataLength {
+            //             len: data.remaining(),
+            //         });
+            //     }
+            //     while data.has_remaining() {
+            //         eir.uuid128.push(data.get_u128_le());
+            //     }
+            // }
             Some(EIRDataTypes::NameShort) => {
-                if eir.name.is_some() {
+                if has_name {
                     return Err(EIRError::RepeatedName);
                 }
-                eir.name = Some(EIRName::short_name(
+                has_name = true;
+                eir.push(EIR::Name(
                     String::from_utf8_lossy(data.bytes()).to_string(),
+                    false,
                 ));
             }
             Some(EIRDataTypes::NameComplete) => {
-                if eir.name.is_some() {
+                if has_name {
                     return Err(EIRError::RepeatedName);
                 }
-                eir.name = Some(EIRName::complete_name(
+                has_name = true;
+                eir.push(EIR::Name(
                     String::from_utf8_lossy(data.bytes()).to_string(),
+                    true,
                 ));
             }
-            Some(EIRDataTypes::TxPowerLevel) => {
-                eir.tx_power_level.push(data.get_i8());
-            }
-            Some(EIRDataTypes::URI) => {
-                let uri_scheme = data.get_u8();
-                if uri_scheme == 0x01 {
-                    let uri = String::from_utf8(data.bytes().to_vec());
-                    if uri.is_err() {
-                        return Err(EIRError::InvalidURI);
-                    }
-                    eir.uri.push(uri.unwrap());
-                } else {
-                    // TODO: URI scheme translation. Skip for now.
-                }
-            }
-            Some(EIRDataTypes::ManufacturerSpecificData) => {
-                if data.remaining() < 2 {
-                    return Err(EIRError::UnexpectedDataLength {
-                        len: data.remaining(),
-                    });
-                }
-                eir.manufacturer_specific_data
-                    .push(ManufacturerSpecificData {
-                        company_identifier_code: data.get_u16_le(),
-                        data: Bytes::copy_from_slice(data.bytes()),
-                    });
-            }
+            // Some(EIRDataTypes::TxPowerLevel) => {
+            //     eir.tx_power_level.push(data.get_i8());
+            // }
+            // Some(EIRDataTypes::URI) => {
+            //     let uri_scheme = data.get_u8();
+            //     if uri_scheme == 0x01 {
+            //         let uri = String::from_utf8(data.bytes().to_vec());
+            //         if uri.is_err() {
+            //             return Err(EIRError::InvalidURI);
+            //         }
+            //         eir.uri.push(uri.unwrap());
+            //     } else {
+            //         // TODO: URI scheme translation. Skip for now.
+            //     }
+            // }
+            // Some(EIRDataTypes::ManufacturerSpecificData) => {
+            //     if data.remaining() < 2 {
+            //         return Err(EIRError::UnexpectedDataLength {
+            //             len: data.remaining(),
+            //         });
+            //     }
+            //     eir.manufacturer_specific_data
+            //         .push(ManufacturerSpecificData {
+            //             company_identifier_code: data.get_u16_le(),
+            //             data: Bytes::copy_from_slice(data.bytes()),
+            //         });
+            // }
             _ => {
                 // Skip unknown data
             }
@@ -234,41 +233,37 @@ mod tests {
         let eir = parse_eir(input);
         assert!(eir.is_ok());
         let eir = eir.unwrap();
-        assert!(eir.flags.is_none());
-        assert!(eir.uuid16.is_empty());
-        assert!(eir.uuid32.is_empty());
-        assert!(eir.uuid128.is_empty());
-        assert!(eir.name.is_some());
-        let name = eir.name.unwrap();
-        assert!(name.complete);
-        assert_eq!(name.name, "ABC");
-        assert!(eir.tx_power_level.is_empty());
-        assert!(eir.uri.is_empty());
-        assert!(eir.manufacturer_specific_data.is_empty());
+        assert_eq!(eir.len(), 1);
+        if let EIR::Name(name, complete) = &eir[0] {
+            assert_eq!(name, "ABC");
+            assert!(!complete);
+        } else {
+            assert!(false);
+        }
     }
 
-    #[test]
-    pub fn eir_multiple_test() {
-        let input = Bytes::copy_from_slice(b"\x02\x01\x06\x03\x03\xAB\xAC\x03\x08Hi");
-        let eir = parse_eir(input);
-        assert!(eir.is_ok());
-        let eir = eir.unwrap();
-        assert!(eir.flags.is_some());
-        let flags = eir.flags.unwrap();
-        assert_eq!(
-            flags,
-            EIRFlags::BREDRNotSupported | EIRFlags::LEGeneralDiscoverableMode
-        );
-        assert!(!eir.uuid16.is_empty());
-        assert_eq!(eir.uuid16, vec![0xACAB]);
-        assert!(eir.uuid32.is_empty());
-        assert!(eir.uuid128.is_empty());
-        assert!(eir.name.is_some());
-        let name = eir.name.unwrap();
-        assert!(!name.complete);
-        assert_eq!(name.name, "Hi");
-        assert!(eir.tx_power_level.is_empty());
-        assert!(eir.uri.is_empty());
-        assert!(eir.manufacturer_specific_data.is_empty());
-    }
+    // #[test]
+    // pub fn eir_multiple_test() {
+    //     let input = Bytes::copy_from_slice(b"\x02\x01\x06\x03\x03\xAB\xAC\x03\x08Hi");
+    //     let eir = parse_eir(input);
+    //     assert!(eir.is_ok());
+    //     let eir = eir.unwrap();
+    //     assert!(eir.flags.is_some());
+    //     let flags = eir.flags.unwrap();
+    //     assert_eq!(
+    //         flags,
+    //         EIRFlags::BREDRNotSupported | EIRFlags::LEGeneralDiscoverableMode
+    //     );
+    //     assert!(!eir.uuid16.is_empty());
+    //     assert_eq!(eir.uuid16, vec![0xACAB]);
+    //     assert!(eir.uuid32.is_empty());
+    //     assert!(eir.uuid128.is_empty());
+    //     assert!(eir.name.is_some());
+    //     let name = eir.name.unwrap();
+    //     assert!(!name.complete);
+    //     assert_eq!(name.name, "Hi");
+    //     assert!(eir.tx_power_level.is_empty());
+    //     assert!(eir.uri.is_empty());
+    //     assert!(eir.manufacturer_specific_data.is_empty());
+    // }
 }
